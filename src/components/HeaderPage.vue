3951d794a9ffc885b5572432febfacb1e399e413
maelontheflux <mael.grosjean@edu.univ-fcomte.fr>
2024-02-26 19:05:36 +0100

    test

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -4,6 +4,11 @@ import { currentTime } from '@/backend'
 </script>
 
 <template>
-  <div>{{ currentTime }}</div>
-
+  <div class="flex justify-between">
+    <div>Maël Grosjean</div>
+    <div>{{ currentTime }}</div>
+    <div class="menu__nav-toggle" onclick="document.body.classList.toggle('menu-open')" tabindex="0">
+      <div class="menu__nav-toggle-bar"></div>
+    </div>
+  </div>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,15 +5,33 @@
 
 <template>
 
-  <div class="mask">
-    <img src="/fentezi-planet.jpeg" alt="">
+  <div class="parent">
+    <div class="rectangle"></div>
+    <div class="mask">
+      <img src="/Group_15420.png" alt="">
+    </div>
   </div>
 
 </template>
 
 <style scoped>
   .mask {
-    mask-image: url('/round-outline-ribbon-illustration-isolated-on-transparent-background-free-png.webp');
+    mask-image: url('/test.png');
+    mask-size: 100%;
     mask-repeat: no-repeat;
+  }
+  .parent {
+    position: relative;
+    contain: content;
+    background-image: url('/Group_15420.png');
+    max-width: 100%;
+  }
+  .rectangle {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 80%;
+    background-color: #fff;
   }
 </style>
