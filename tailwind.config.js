7d3d2fa9b75b21fd5a62d6c36251a866f7f1246f
maelontheflux <mael.grosjean@edu.univ-fcomte.fr>
2024-02-26 20:49:45 +0100

    style desktop

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "secondary-white": "#edeae6",
+      "main-black": "#141414",
+      "dynamic-gray": "#333333"
+    },
+    fontSize: {
+      "base": "1.375rem",
+      "lg": "3.5rem",
+      "xl": "5rem",
+      "2xl": "8.1875rem"
+    },
+    fontFamily: {
+      "uncut-sans": "Uncut Sans"
+    },
     extend: {},
   },
   plugins: [],
