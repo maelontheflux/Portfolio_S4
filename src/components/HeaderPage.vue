c9a2bc362273516cc01e5eb7c98be3ae16bc5a71
maelontheflux <mael.grosjean@edu.univ-fcomte.fr>
2024-02-27 21:56:09 +0100

    menu hamburger v1

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,32 +1,40 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
 import { currentTime } from '@/backend'
 
+// Variable etat du menu
+const activeMenu = ref(false)
+// Fonction pour ouvrir/fermer le menu
+function toggleMenu() {
+    activeMenu.value = !activeMenu.value;
+    document.body.classList.toggle('menu-open');
+}
 </script>
 
 <template>
   <div class="p-5 flex justify-between items-center">
     <div>Maël Grosjean</div>
     <div>{{ currentTime }}</div>
-    <div class="menu__parent cursor-pointer m-2" onclick="document.body.classList.toggle('menu-open')" tabindex="0">
+    <div class="menu__parent cursor-pointer m-2" @click="toggleMenu" tabindex="0">
       <div class="menu__nav-toggle">
         <div class="menu__nav-toggle-bar"></div>
       </div>
     </div>
   </div>
   <nav class="menu bg-secondary-white">
-    <ul class="menu menu__list">
+    <ul class="menu menu__list" v-scroll-lock="activeMenu">
       <li class="menu__item text-xl">
-        <RouterLink to="" class="menu__link">Projets</RouterLink>
+        <RouterLink to="" class="menu__link" @click="toggleMenu">Projets</RouterLink>
       </li>
       <li class="menu__item text-xl">
-        <RouterLink to="" class="menu__link">About</RouterLink>
+        <RouterLink to="" class="menu__link" @click="toggleMenu">About</RouterLink>
       </li>
       <li class="menu__item text-xl">
-        <RouterLink to="" class="menu__link">Notes</RouterLink>
+        <RouterLink to="" class="menu__link" @click="toggleMenu">Notes</RouterLink>
       </li>
       <li class="menu__item text-xl">
-        <a href="mailto:mael.gsn01@gmail.com" class="menu__link underline">Contact</a>
+        <a href="mailto:mael.gsn01@gmail.com" class="menu__link underline" @click="toggleMenu">Contact</a>
       </li>
     </ul>
     <div class="menu__center-pts bg-secondary-white"></div>
