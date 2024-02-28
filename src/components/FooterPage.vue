1cac082159ebf748918e1dda2a8180ed2649b4fa
maelontheflux <mael.grosjean@edu.univ-fcomte.fr>
2024-02-28 19:06:28 +0100

    footer v1

--- src/components/FooterPage.vue
+++ src/components/FooterPage.vue
@@ -1,33 +1,45 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import { currentTime } from '@/backend'
 
 import icon_world from '@/components/icons/icon_world.vue'
 
+// Variable etat du menu
+const activeMenu = ref(false)
+// Fonction pour ouvrir/fermer le menu
+function activateMenu() {
+    activeMenu.value = true;
+}
+function deactivateMenu() {
+    activeMenu.value = false;
+}
 </script>
 
 <template>
-  <div class="footer__class font-semibold">
-    <div class="justify-end font-medium text-lg ml-2 rotate-[-90deg] origin-left flex items-center gap-2">
-      <icon_world class="w-8 h-8" />
-      {{ currentTime }}
+  <div class="footer__class font-semibold relative contents">
+    <div class="absolute top-36 sm:top-44 left-10">
+      <div class="font-medium sm:text-lg sm:ml-2 rotate-[-90deg] origin-left flex items-center gap-2">
+        <icon_world class="w-6 h-6 sm:w-8 sm:h-8" />
+        {{ currentTime }}
+      </div>
     </div>
-    <div class="grid mt-[10%] mb-[15%] lg:grid-cols-2 lg:gap-20">
-      <div class="text-[80px]">
+    <div class="grid ml-[22%] mt-[8%] mb-[15%] lg:grid-cols-2 lg:gap-20">
+      <div class="text-[50px] sm:text-[80px]">
         <p>Maël</p>
         <p class="-mt-6">Grosjean</p>
       </div>
-      <div class="text-base self-end lg:mt-[40%]">
+      <div class="sm:text-base self-end lg:mt-[40%]">
         <p>Want to know more or have a</p>
-        <p class="-mt-2 -mb-2">project in mind ?</p>
+        <p class="-mt-1 -mb-1 sm:-mt-2 sm:-mb-2">project in mind ?</p>
         <a href="mailto:mael.gsn01@gmail.com" class="underline">Let's Connect</a>
       </div>
     </div>
-    <div class="text-end">
-      <span>EN</span>
-      <div class="mt-[100%]">
-        <p class="text-start origin-right rotate-[-90deg]">FOLLOW THE CONTENT</p>
-        <nav>
-          <ul>
+    <div class="text-end relative">
+      <span class="sm:text-base font-medium mr-2">EN</span>
+      <div class="w-max absolute sm:bottom-0 right-0 items-center">
+        <p class="cursor-pointer text-start origin-bottom-right rotate-[-90deg] sm:text-base ease-in duration-500 opacity-100" @mouseenter="activateMenu" @mouseleave="deactivateMenu" :class="{ '!opacity-0' : activeMenu }">FOLLOW THE CONTENT</p>
+        <nav class="invisible opacity-0 ease-in duration-500" :class="{ '!visible opacity-100' : activeMenu }">
+          <ul class="underline grid gap-6 sm:gap-12" @mouseenter="activateMenu" @mouseleave="deactivateMenu">
             <li>
               <a href="">instagram</a>
             </li>
@@ -46,7 +58,7 @@ import icon_world from '@/components/icons/icon_world.vue'
     </div>
   </div>
 
-  <div class="p-5 pl-[22%] font-semibold">
+  <div class="max-sm:pt-0 pb-5 p-5 pl-[22%] font-semibold">
     <a href="">DOWNLOAD MY RESUME</a>
   </div>
 </template>
