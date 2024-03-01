595c609b4fa54b59231da7e0e8cb7f847333fe4c
maelontheflux <mael.grosjean@edu.univ-fcomte.fr>
2024-03-01 19:04:41 +0100

    update

--- src/components/projetPage.vue
+++ src/components/projetPage.vue
@@ -43,54 +43,57 @@ function handleMouseEnter(index: number) {
 function handleMouseLeave() {
     for (let i = 1; i <= 6; i++) {
         document.getElementById(`img${i}`)?.classList.remove('animate-hover');
+        document.getElementById(`img--scd${i}`)?.classList.remove('animate-hover');
+    }
+}
+function handleMouseEnterScd(index: number) {
+    for (let i = 1; i <= 6; i++) {
+        if (i !== index) {
+            document.getElementById(`img--scd${i}`)?.classList.add('animate-hover');
+        }
     }
 }
 // //
 </script>
 <template>
     <div class="project__page">
-        <img :src="urlImg_hero" alt="">
-        <div class="px-14">
-            <div class="text-center sm:text-xl" @mouseenter="shuffle($event.target)">{{ props.name }}</div>
-            <div class="" >HELP</div>
+        <img :src="urlImg_hero" alt="Image d'illustration principal du projet">
+        <div class="px-4 sm:px-14">
+            <div class="text-center font-bold sm:mt-14 sm:text-[75px]" @mouseenter="shuffle($event.target)">{{ props.name }}</div>
             <div class="text-end">
-                <div>{{ props.type }}</div>
-                <div>{{ props.categorie }}</div>
-                <div>{{ props.date }}</div>
+                <div class="">{{ props.type }}</div>
+                <div class="-mt-1">{{ props.categorie }}</div>
+                <div class="-mt-1">{{ props.date }}</div>
             </div>
-            <div>{{ props.description1 }}</div>
+            <div class="max-w-[1110px]">{{ props.description1 }}</div>
             <div class="text-end">{{ props.description2 }}</div>
-            <div class="flex sm:w-[70%] lg:w-[50%]">
-                <img id="img1" class="animate--anim opacity-100 z-40 shadow-xl" :src="urlImg1_1" alt=""
+            <div class="flex w-[70%] lg:w-[50%]">
+                <img id="img1" class="animate--anim opacity-100 z-40 drop-shadow-2xl" :src="urlImg1_1" alt="Image du projet"
                     @mouseenter="handleMouseEnter(1)" @mouseleave="handleMouseLeave">
-                <img id="img2" class="animate--anim opacity-100 z-30 -ml-[90%] shadow-xl" :src="urlImg1_2" alt=""
+                <img id="img2" class="animate--anim opacity-100 z-30 -ml-[90%] drop-shadow-2xl" :src="urlImg1_2" alt="Image du projet"
                     @mouseenter="handleMouseEnter(2)" @mouseleave="handleMouseLeave">
-                <img id="img3" class="animate--anim opacity-100 z-20 -ml-[90%] shadow-xl" :src="urlImg1_3" alt=""
+                <img id="img3" class="animate--anim opacity-100 z-20 -ml-[90%] drop-shadow-2xl" :src="urlImg1_3" alt="Image du projet"
                     @mouseenter="handleMouseEnter(3)" @mouseleave="handleMouseLeave">
-                <img id="img4" class="animate--anim opacity-100 z-10 -ml-[90%] shadow-xl" :src="urlImg1_4" alt=""
+                <img id="img4" class="animate--anim opacity-100 z-10 -ml-[90%] drop-shadow-2xl" :src="urlImg1_4" alt="Image du projet"
                     @mouseenter="handleMouseEnter(4)" @mouseleave="handleMouseLeave">
-                <img id="img5" class="animate--anim opacity-100 -ml-[90%] shadow-xl" :src="urlImg1_5" alt=""
+                <img id="img5" class="animate--anim opacity-100 -ml-[90%] drop-shadow-2xl" :src="urlImg1_5" alt="Image du projet"
                     @mouseenter="handleMouseEnter(5)" @mouseleave="handleMouseLeave">
             </div>
             <div>{{ description3 }}</div>
-            <div class="flex text-end">
-                <img class="z-50 shadow-xl" :src="urlImg2_1" alt="">
-                <img class="z-40 shadow-xl" :src="urlImg2_2" alt="">
-                <img class="z-30 shadow-xl" :src="urlImg2_3" alt="">
-                <img class="z-20 shadow-xl" :src="urlImg2_4" alt="">
-                <img class="z-10 shadow-xl" :src="urlImg2_5" alt="">
-                <img class="shadow-xl" :src="urlImg2_6" alt="">
+            <div class="flex w-[60%] lg:w-[40%]">
+                <img id="img--scd1" class="animate--anim opacity-100 z-50 drop-shadow-2xl" :src="urlImg2_1" alt="Image du projet"
+                    @mouseenter="handleMouseEnterScd(1)" @mouseleave="handleMouseLeave">
+                <img id="img--scd2" class="animate--anim opacity-100 z-40 -ml-[88%] drop-shadow-2xl" :src="urlImg2_2" alt="Image du projet"
+                    @mouseenter="handleMouseEnterScd(2)" @mouseleave="handleMouseLeave">
+                <img id="img--scd3" class="animate--anim opacity-100 z-30 -ml-[88%] drop-shadow-2xl" :src="urlImg2_3" alt="Image du projet"
+                    @mouseenter="handleMouseEnterScd(3)" @mouseleave="handleMouseLeave">
+                <img id="img--scd4" class="animate--anim opacity-100 z-20 -ml-[88%] drop-shadow-2xl" :src="urlImg2_4" alt="Image du projet"
+                    @mouseenter="handleMouseEnterScd(4)" @mouseleave="handleMouseLeave">
+                <img id="img--scd5" class="animate--anim opacity-100 z-10 -ml-[88%] drop-shadow-2xl" :src="urlImg2_5" alt="Image du projet"
+                    @mouseenter="handleMouseEnterScd(5)" @mouseleave="handleMouseLeave">
+                <img id="img--scd6" class="animate--anim opacity-100 -ml-[88%] drop-shadow-2xl" :src="urlImg2_6" alt="Image du projet"
+                    @mouseenter="handleMouseEnterScd(6)" @mouseleave="handleMouseLeave">
             </div>
         </div>
     </div>
 </template>
-
-<style scoped>
-.animate-hover {
-    opacity: 0;
-    
-}
-.animate--anim {
-    transition: opacity 1s ease;
-}
-</style>
