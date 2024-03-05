8e1c4ef7334d1b68f455e1dab25c9860d6edc9c6
maelontheflux <mael.grosjean@edu.univ-fcomte.fr>
2024-03-05 17:27:42 +0100

    new page

---
+++ src/pages/about.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import Arrow from '@/components/icons/icon_arrow.vue'
+
+</script>
+
+<template>
+    <div>
+        <p>Hello, my name is <span>Maël</span> Grosjean, student designer
+            based in Montbéliard and Le Locle.
+            I create brand identities, design websites.
+        </p>
+    </div>
+    <div>
+        <p>In addition to being a <strong>design fan</strong>, I enjoy taking simple <strong>photos</strong> and <strong>videos</strong>, but most of all I like listening to <strong>music</strong> and I'm particularly interested in <strong>album covers</strong>.</p>
+    </div>
+    <div class="flex">
+        <p>What's keeping you?</p>
+        <Arrow />
+        <RouterLink to="/contact" class="underline">CONTACT ME</RouterLink>
+    </div>
+</template>
