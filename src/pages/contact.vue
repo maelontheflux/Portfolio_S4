98e55f6644872a663921910ee6f8f6b174abaf7a
maelontheflux <mael.grosjean@edu.univ-fcomte.fr>
2024-03-06 17:48:17 +0100

    responsive contact

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -3,16 +3,16 @@
 </script>
 
 <template>
-    <div class="bg-main-black text-secondary-white p-20">
-        <div class="text-center text-[40px]">
+    <div class="bg-main-black text-secondary-white p-6 sm:p-20">
+        <div class="font-medium md:text-center text-base sm:text-[40px]">
             <h1>Let’s connect and creating together !</h1>
-            <p class="">Contact me and tell me about your project</p>
+            <p class="mt-2 md:mt-5 sm:mb-4">Contact me and tell me about your project</p>
         </div>
 
-        <form class="grid justify-center text-base p-10">
+        <form class="grid justify-center sm:text-base max-md:py-10 md:p-10">
             <ul class="">
                 <label for="ydata">Your Data</label>
-                <div id="ydata" class="grid gap-5 sm:grid-cols-2 sm:gap-20">
+                <div id="ydata" class="grid gap-5 sm:grid-cols-2 sm:gap-20 mt-2 sm:mt-4">
                     <li class="grid">
                         <input name="name" type="text" class="input__contact" placeholder="Name*">
                     </li>
@@ -21,8 +21,8 @@
                     </li>
                 </div>
                 <li class="grid">
-                    <label for="type" class="mb-5 sm:mt-12">Your are interested in</label>
-                    <ul id="type" class="flex flex-wrap gap-10">
+                    <label for="type" class="mb-4 sm:mb-5 mt-8 sm:mt-12">Your are interested in</label>
+                    <ul id="type" class="flex flex-wrap gap-4 sm:gap-10">
                         <li class="list__input">
                             <input type="checkbox" id="interest1" name="" value="option1" class="input__contact--checkbox">
                             <label for="interest1" class="label__contact">UX/UI Design</label>
@@ -46,11 +46,11 @@
                     </ul>
                 </li>
                 <li class="grid">
-                    <label for="project" class="mb-2 sm:mt-12">Your project is</label>
+                    <label for="project" class="mb-2 sm:mb-4 mt-8 sm:mt-12">Your project is</label>
                     <input id="project" name="project" type="text" class="input__contact" placeholder="Project details">
                 </li>
             </ul>
-            <button type="submit" class="md:max-w-[20%] mx-auto mt-10">Submit Message</button>
+            <button type="submit" class="md:max-w-[20%] mx-auto mt-10 sm:mt-14">Submit Message</button>
         </form>
     </div>
 </template>
